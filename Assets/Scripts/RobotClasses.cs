789a3763a8899e2edbafae0812e46849290e3bb8
nathandunk <nbd2@rice.edu>
2019-01-10 21:13:07 -0600

    pushing to retrieve elsewhere

--- Assets/Scripts/RobotClasses.cs
+++ Assets/Scripts/RobotClasses.cs
@@ -8,30 +8,50 @@ public class RobotClasses : MonoBehaviour {
     {
         public string JointType;
 
-        public Joint(string JointType_)
+        public Joint(string JointType_, double alpha, double a, double d, double theta)
         {
-            JointType = JointType_;
+            this.JointType = JointType_;
         }
     }
 
     public class Robot
     {
-        public string[] Joints;
+        public List<Joint> Joints = new List<Joint>();
+        public List<string> JointTypes = new List<string>();
         public string[] JointTypes;
-        public float[]  alpha;
-        public float[]  a;
-        public float[]  d;
-        public float[]  theta;
+        public double[] alpha;
+        public double[] a;
+        public double[] d;
+        public double[] theta;
 
         public Robot(Joint[] joints_)
         {
-            Joints = joints_;
+            this.Joints = joints_;
         }
 
-        public void AddJoint(string type, float alpha, float a, float d, float theta)
+        public void AddJoint(string type, double alpha, double a, double d, double theta)
         {
 
         }
+
+        //private void dhtf(self):
+
+        //Matrix self.T_full = Matrix.Identity
+        //for i in range(0, self.size+1):
+        //    T_hold = np.array([[np.cos(self.joint_values[i]), -np.sin(self.joint_values[i]), 0, self.a[i]],
+        //        [np.sin(self.joint_values[i]) * np.cos(self.alpha[i]), np.cos(self.joint_values[i]) * np.cos(self.alpha[i]), -np.sin(self.alpha[i]), -np.sin(self.alpha[i]) * self.d[i]],
+        //        [np.sin(self.joint_values[i]) * np.sin(self.alpha[i]), np.cos(self.joint_values[i]) * np.sin(self.alpha[i]), np.cos(self.alpha[i]), np.cos(self.alpha[i]) * self.d[i]],
+        //        [0, 0, 0, 1]])
+        //    # print(T_hold.shape)
+        //    # print(T_hold)
+        //    self.T_full = np.matmul(self.T_full, T_hold)
+
+        //    self.O[:, i] = self.T_full[0:3, 3]
+
+        //    self.X[:, i] = self.O[:, i] + self.T_full[0:3, 0] * 10
+        //    self.Y[:, i] = self.O[:, i] + self.T_full[0:3, 1] * 10
+        //    self.Z[:, i] = self.O[:, i] + self.T_full[0:3, 2] * 10
+        //    # print(self.T_full)
     }
 
     // Use this for initialization
